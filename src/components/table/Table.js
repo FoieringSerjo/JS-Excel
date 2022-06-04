e09e7ca7730e266cedae15d92e47bd902a36ad2b
Foiering Sergey <serjofoi@gmail.com>
2022-06-04 16:06:10 +0300

    Finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,25 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 import { createTable } from './table.template';
+import { resizeHandler } from './table.resize';
+import { shouldResize } from './table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
+
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    });
+  }
+
   toHTML() {
     return createTable(20);
+  }
+
+  onMousedown(event) {
+    // console.log(event.target.getAttribute('data-resize'));
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
   }
 }
